505f92989991b7850573d157279f987e81e70d93
likalikabaz <89534939+likalikabaz@users.noreply.github.com>
2023-04-13 16:30:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
-import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 753540521 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    ks_stat, p_value = ks_2samp(x, y)
+    alpha = 0.07
+    critical_value = np.sqrt(-0.5 * np.log(alpha / 2)) / np.sqrt(len(x) + len(y))
+
+    if ks_stat > critical_value:
+        return True
+    else:
+        return False
